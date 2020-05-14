b9db7e87529b60625d11902aebeb02b76a56cf28
hjPark <gmlwnrrj@naver.com>
2020-05-14 19:02:22 +0900

    Greet 컨트롤러 수정
    greeting method 추가

--- initial/src/main/java/com/example/restservice/controller/GreetController.java
+++ initial/src/main/java/com/example/restservice/controller/GreetController.java
@@ -1,4 +1,26 @@
 package com.example.restservice.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/")
 public class GreetController {
+
+    @GetMapping("greeting")
+    public List<Map<String,String>> greeting(){
+
+        List<Map<String,String>> list = new ArrayList<>();
+        Map<String,String> map = new HashMap<>();
+        map.put("greeting", "greeting");
+
+        list.add(map);
+        return list;
+    }
 }
