759696483bd5d6462f9a8fa4c2c6b720b98e5dd0
hjPark <gmlwnrrj@naver.com>
2020-05-15 18:08:20 +0900

    post mothode 추가
    json 등록 양식 받는 메소드 추가

--- initial/src/main/java/com/example/restservice/controller/GreetController.java
+++ initial/src/main/java/com/example/restservice/controller/GreetController.java
@@ -1,19 +1,23 @@
 package com.example.restservice.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.restservice.model.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.*;
 
+import javax.annotation.Resource;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/")
+@RequestMapping("/greet")
 public class GreetController {
 
-    @GetMapping("greeting")
+    private static final Logger logger = LoggerFactory.getLogger(GreetController.class);
+
+    @GetMapping("")
     public List<Map<String,String>> greeting(){
 
         List<Map<String,String>> list = new ArrayList<>();
@@ -23,4 +27,15 @@ public class GreetController {
         list.add(map);
         return list;
     }
+
+    @PostMapping("")
+    public User regUser(@RequestBody User user){
+        logger.info("regUser 등록");
+        logger.info("user_name :" + user.getName());
+        logger.info("user_age :" + user.getAge());
+        logger.info("user_address :" + user.getAddress());
+
+        return user;
+    }
+
 }
